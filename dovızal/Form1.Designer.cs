b11b2d8cd3fb93c81495d0c285766cdbe4ce3956
FAGO6034 <fago3460@gmail.com>
2023-10-10 17:48:38 +0300

    Add project files.

---
+++ dovızal/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace dovızal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DolarAlisText = new System.Windows.Forms.Label();
+            this.DolarSatisText = new System.Windows.Forms.Label();
+            this.EuroSatisText = new System.Windows.Forms.Label();
+            this.EuroAlisText = new System.Windows.Forms.Label();
+            this.btnDolarAlis = new System.Windows.Forms.Button();
+            this.btnDolarSatis = new System.Windows.Forms.Button();
+            this.btnEuroAlis = new System.Windows.Forms.Button();
+            this.btnEuroSatis = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtKUR = new System.Windows.Forms.TextBox();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.txtTUTAR = new System.Windows.Forms.TextBox();
+            this.tXTKALAN = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.btnBozdurma = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(45, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dolar Alış :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(45, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dolar Satış :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(45, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Euro Satış :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(45, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 28);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Euro Alış :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // DolarAlisText
+            // 
+            this.DolarAlisText.BackColor = System.Drawing.Color.Transparent;
+            this.DolarAlisText.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DolarAlisText.ForeColor = System.Drawing.Color.White;
+            this.DolarAlisText.Location = new System.Drawing.Point(225, 41);
+            this.DolarAlisText.Name = "DolarAlisText";
+            this.DolarAlisText.Size = new System.Drawing.Size(121, 28);
+            this.DolarAlisText.TabIndex = 4;
+            this.DolarAlisText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // DolarSatisText
+            // 
+            this.DolarSatisText.BackColor = System.Drawing.Color.Transparent;
+            this.DolarSatisText.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DolarSatisText.ForeColor = System.Drawing.Color.White;
+            this.DolarSatisText.Location = new System.Drawing.Point(225, 103);
+            this.DolarSatisText.Name = "DolarSatisText";
+            this.DolarSatisText.Size = new System.Drawing.Size(121, 28);
+            this.DolarSatisText.TabIndex = 5;
+            this.DolarSatisText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // EuroSatisText
+            // 
+            this.EuroSatisText.BackColor = System.Drawing.Color.Transparent;
+            this.EuroSatisText.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.EuroSatisText.ForeColor = System.Drawing.Color.White;
+            this.EuroSatisText.Location = new System.Drawing.Point(225, 227);
+            this.EuroSatisText.Name = "EuroSatisText";
+            this.EuroSatisText.Size = new System.Drawing.Size(121, 28);
+            this.EuroSatisText.TabIndex = 7;
+            this.EuroSatisText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // EuroAlisText
+            // 
+            this.EuroAlisText.BackColor = System.Drawing.Color.Transparent;
+            this.EuroAlisText.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.EuroAlisText.ForeColor = System.Drawing.Color.White;
+            this.EuroAlisText.Location = new System.Drawing.Point(225, 165);
+            this.EuroAlisText.Name = "EuroAlisText";
+            this.EuroAlisText.Size = new System.Drawing.Size(121, 28);
+            this.EuroAlisText.TabIndex = 6;
+            this.EuroAlisText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnDolarAlis
+            // 
+            this.btnDolarAlis.Location = new System.Drawing.Point(367, 37);
+            this.btnDolarAlis.Name = "btnDolarAlis";
+            this.btnDolarAlis.Size = new System.Drawing.Size(82, 38);
+            this.btnDolarAlis.TabIndex = 8;
+            this.btnDolarAlis.Text = "...";
+            this.btnDolarAlis.UseVisualStyleBackColor = true;
+            this.btnDolarAlis.Click += new System.EventHandler(this.btnDolarAlis_Click);
+            // 
+            // btnDolarSatis
+            // 
+            this.btnDolarSatis.Location = new System.Drawing.Point(367, 96);
+            this.btnDolarSatis.Name = "btnDolarSatis";
+            this.btnDolarSatis.Size = new System.Drawing.Size(82, 40);
+            this.btnDolarSatis.TabIndex = 9;
+            this.btnDolarSatis.Text = "...";
+            this.btnDolarSatis.UseVisualStyleBackColor = true;
+            this.btnDolarSatis.Click += new System.EventHandler(this.btnDolarSatis_Click);
+            // 
+            // btnEuroAlis
+            // 
+            this.btnEuroAlis.Location = new System.Drawing.Point(367, 157);
+            this.btnEuroAlis.Name = "btnEuroAlis";
+            this.btnEuroAlis.Size = new System.Drawing.Size(82, 38);
+            this.btnEuroAlis.TabIndex = 10;
+            this.btnEuroAlis.Text = "...";
+            this.btnEuroAlis.UseVisualStyleBackColor = true;
+            this.btnEuroAlis.Click += new System.EventHandler(this.btnEuroAlis_Click);
+            // 
+            // btnEuroSatis
+            // 
+            this.btnEuroSatis.Location = new System.Drawing.Point(367, 216);
+            this.btnEuroSatis.Name = "btnEuroSatis";
+            this.btnEuroSatis.Size = new System.Drawing.Size(89, 38);
+            this.btnEuroSatis.TabIndex = 11;
+            this.btnEuroSatis.Text = "...";
+            this.btnEuroSatis.UseVisualStyleBackColor = true;
+            this.btnEuroSatis.Click += new System.EventHandler(this.btnEuroSatis_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btnBozdurma);
+            this.groupBox1.Controls.Add(this.btnHesapla);
+            this.groupBox1.Controls.Add(this.tXTKALAN);
+            this.groupBox1.Controls.Add(this.txtTUTAR);
+            this.groupBox1.Controls.Add(this.txtMiktar);
+            this.groupBox1.Controls.Add(this.txtKUR);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(529, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(474, 348);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Doviz İşlemler";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Location = new System.Drawing.Point(6, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 28);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "KALAN :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label6.Location = new System.Drawing.Point(6, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 28);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "TUTAR :";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label7.Location = new System.Drawing.Point(6, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 28);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "MIKTAR:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label8.Location = new System.Drawing.Point(6, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 28);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "KUR:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtKUR
+            // 
+            this.txtKUR.Location = new System.Drawing.Point(130, 31);
+            this.txtKUR.Name = "txtKUR";
+            this.txtKUR.Size = new System.Drawing.Size(316, 32);
+            this.txtKUR.TabIndex = 8;
+            this.txtKUR.TextChanged += new System.EventHandler(this.txtKUR_TextChanged);
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.Location = new System.Drawing.Point(130, 94);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.Size = new System.Drawing.Size(316, 32);
+            this.txtMiktar.TabIndex = 9;
+            // 
+            // txtTUTAR
+            // 
+            this.txtTUTAR.Location = new System.Drawing.Point(130, 157);
+            this.txtTUTAR.Name = "txtTUTAR";
+            this.txtTUTAR.Size = new System.Drawing.Size(316, 32);
+            this.txtTUTAR.TabIndex = 10;
+            // 
+            // tXTKALAN
+            // 
+            this.tXTKALAN.Location = new System.Drawing.Point(130, 220);
+            this.tXTKALAN.Name = "tXTKALAN";
+            this.tXTKALAN.Size = new System.Drawing.Size(316, 32);
+            this.tXTKALAN.TabIndex = 11;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.ForeColor = System.Drawing.Color.Maroon;
+            this.btnHesapla.Location = new System.Drawing.Point(10, 281);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(201, 38);
+            this.btnHesapla.TabIndex = 12;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // btnBozdurma
+            // 
+            this.btnBozdurma.ForeColor = System.Drawing.Color.Maroon;
+            this.btnBozdurma.Location = new System.Drawing.Point(235, 281);
+            this.btnBozdurma.Name = "btnBozdurma";
+            this.btnBozdurma.Size = new System.Drawing.Size(211, 38);
+            this.btnBozdurma.TabIndex = 13;
+            this.btnBozdurma.Text = "Bozdurma";
+            this.btnBozdurma.UseVisualStyleBackColor = true;
+            this.btnBozdurma.Click += new System.EventHandler(this.btnBozdurma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1048, 520);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnEuroSatis);
+            this.Controls.Add(this.btnEuroAlis);
+            this.Controls.Add(this.btnDolarSatis);
+            this.Controls.Add(this.btnDolarAlis);
+            this.Controls.Add(this.EuroSatisText);
+            this.Controls.Add(this.EuroAlisText);
+            this.Controls.Add(this.DolarSatisText);
+            this.Controls.Add(this.DolarAlisText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Döviz kuru islemleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label DolarAlisText;
+        private System.Windows.Forms.Label DolarSatisText;
+        private System.Windows.Forms.Label EuroSatisText;
+        private System.Windows.Forms.Label EuroAlisText;
+        private System.Windows.Forms.Button btnDolarAlis;
+        private System.Windows.Forms.Button btnDolarSatis;
+        private System.Windows.Forms.Button btnEuroAlis;
+        private System.Windows.Forms.Button btnEuroSatis;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtKUR;
+        private System.Windows.Forms.TextBox tXTKALAN;
+        private System.Windows.Forms.TextBox txtTUTAR;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Button btnBozdurma;
+    }
+}
+
---
+++ dovızal/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace dovızal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+           var xmldosya = new XmlDocument();
+            xmldosya.Load(bugun);
+
+            string DolarAlis = xmldosya.SelectSingleNode("Tarih_Date / Currency[@Kod='USD']/BanknoteBuying").InnerXml;
+            this.DolarAlisText.Text = DolarAlis;
+
+            string DolarSatis = xmldosya.SelectSingleNode("Tarih_Date / Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            this.DolarSatisText.Text = DolarSatis;
+
+            string EuroAlis = xmldosya.SelectSingleNode("Tarih_Date / Currency[@Kod='EUR']/BanknoteBuying").InnerXml;
+            this.EuroAlisText.Text = EuroAlis;
+
+            string EuroSatis = xmldosya.SelectSingleNode("Tarih_Date / Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+            this.EuroSatisText.Text = EuroSatis;
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDolarAlis_Click(object sender, EventArgs e)
+        {
+            txtKUR.Text = DolarAlisText.Text;
+        }
+
+        private void btnDolarSatis_Click(object sender, EventArgs e)
+        {
+            txtKUR.Text = DolarSatisText.Text;
+        }
+
+        private void btnEuroAlis_Click(object sender, EventArgs e)
+        {
+            txtKUR.Text = EuroAlisText.Text;
+        }
+
+        private void btnEuroSatis_Click(object sender, EventArgs e)
+        {
+            txtKUR.Text = EuroSatisText.Text;
+        }
+
+
+        double kur, miktar, tutar;
+
+        private void btnBozdurma_Click(object sender, EventArgs e)
+        {
+            kur=Convert.ToDouble(txtKUR.Text);
+            int miktar=Convert.ToInt32(txtMiktar.Text);
+            int Tutar = Convert.ToInt32(miktar / kur);
+            txtTUTAR.Text= Tutar.ToString();
+            double kalan;
+            kalan =miktar % kur;
+            tXTKALAN.Text= kalan.ToString();
+        }
+
+        private void txtKUR_TextChanged(object sender, EventArgs e)
+        {
+            txtKUR.Text=txtKUR.Text.Replace(".",",");
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            kur=Convert.ToDouble(txtKUR.Text);
+            miktar=Convert.ToDouble(txtMiktar.Text);
+            tutar = miktar*kur;
+
+            txtTUTAR.Text=tutar.ToString();
+        }
+    }
+}
